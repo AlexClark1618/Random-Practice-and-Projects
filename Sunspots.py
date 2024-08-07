4492deedbd53ba291a9f83900668bf08045b52aa
Alex Clark <alexclark1618@gmail.com>
2024-08-07 13:19:36 -0700

    Work in progress

--- Sunspots.py
+++ Sunspots.py
@@ -15,10 +15,22 @@ month_data = [tup[0] for tup in sunspot_month_data_list]
 sunspot_number_data = [tup[1] for tup in sunspot_month_data_list]
 
 plt.plot(month_data, sunspot_number_data)
-plt.xlabel("Month (form Jan 1749)")
+plt.xlabel("Month (Starting form Jan 1749)")
 plt.ylabel("Sunspot Occurance")
 plt.title("Sunspot Occurance Per Month")
 plt.show()
+
+def running_average(data: list, r: "radius"):
+
+    running_avg_data=[]
+
+    for i in range(len(sunspot_number_data)):
+
+        try:
+            sum(sunspot_number_data[-r+i, r+i+1])
+        if sunspot_number_data.index(data) < r or sunspot_number_data.index(data) > (len(sunspot_number_data) - r):
+
+    for i in range(-r, r+1):
     
 
     
